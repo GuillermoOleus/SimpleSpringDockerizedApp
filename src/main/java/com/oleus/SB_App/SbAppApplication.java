7c4b5536162c1efb448c5186b16db22e88ffdebb
GuillermoOleus <guillermo.hernandez.dev@gmail.com>
2024-01-01 22:02:57 -0600

    Hello Docker with Spring Boot

---
+++ src/main/java/com/oleus/SB_App/SbAppApplication.java
@@ -0,0 +1,13 @@
+package com.oleus.SB_App;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbAppApplication.class, args);
+	}
+
+}
